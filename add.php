0c0464b6c1ac576e24b5d9f8ae50be428dfd72b7
luciano <lucianoleal.97@hotmail.com>
2021-02-12 10:13:13 -0300

    mi primer commit

---
+++ add.php
@@ -0,0 +1,113 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<!--
+Project      : Datos de empleados con PHP, MySQLi y Bootstrap CRUD  (Create, read, Update, Delete) 
+Author		 : Obed Alvarado
+Website		 : http://www.obedalvarado.pw
+Blog         : https://obedalvarado.pw/blog/
+Email	 	 : info@obedalvarado.pw
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latihan MySQLi</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include("nav.php");?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Datos del usuario &raquo; Agregar datos</h2>
+			<hr />
+
+			<?php
+			if(isset($_POST['add'])){
+				$id		     = mysqli_real_escape_string($con,(strip_tags($_POST["id"],ENT_QUOTES)));//Escanpando caracteres 
+				$nombre		     = mysqli_real_escape_string($con,(strip_tags($_POST["nombre"],ENT_QUOTES)));//Escanpando caracteres 
+				$mail	 = mysqli_real_escape_string($con,(strip_tags($_POST["mail"],ENT_QUOTES)));//Escanpando caracteres 
+				$usuario	 = mysqli_real_escape_string($con,(strip_tags($_POST["usuario"],ENT_QUOTES)));//Escanpando caracteres 
+							
+			
+
+				$cek = mysqli_query($con, "SELECT * FROM usuarios WHERE id='$id'");
+				if(mysqli_num_rows($cek) == 0){
+						$insert = mysqli_query($con, "INSERT INTO usuarios(id, nombre, mail, usuario)
+															VALUES('$id','$nombre','$mail','$usuario')") or die(mysqli_error());
+						if($insert){
+							echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Bien hecho! Los datos han sido guardados con éxito.</div>';
+						}else{
+							echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. No se pudo guardar los datos !</div>';
+						}
+					 
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. código exite!</div>';
+				}
+			}
+			?>
+
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Id</label>
+					<div class="col-sm-2">
+						<input type="text" name="id" class="form-control" placeholder="Id" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombre</label>
+					<div class="col-sm-4">
+						<input type="text" name="nombre" class="form-control" placeholder="Nombre" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Mail</label>
+					<div class="col-sm-4">
+						<input type="text" name="mail" class="form-control" placeholder="Mail" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Usuario</label>
+					<div class="col-sm-3">
+						<input type="text" name="usuario" class="form-control" placeholder="usuario" required>
+					</div>
+				</div>				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="add" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+	$('.date').datepicker({
+		format: 'dd-mm-yyyy',
+	})
+	</script>
+</body>
+</html>
---
+++ edit.php
@@ -0,0 +1,116 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<!--
+Project      : Datos de empleados con PHP, MySQLi y Bootstrap CRUD  (Create, read, Update, Delete) 
+Author		 : Obed Alvarado
+Website		 : http://www.obedalvarado.pw
+Blog         : https://obedalvarado.pw/blog/
+Email	 	 : info@obedalvarado.pw
+-->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Datos de empleados</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+	
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include("nav.php");?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Datos del usuario &raquo; Editar datos</h2>
+			<hr />
+			
+			<?php
+			// escaping, additionally removing everything that could be (html/javascript-) code
+			$nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
+			$sql = mysqli_query($con, "SELECT * FROM usuarios WHERE id='$nik'");
+			if(mysqli_num_rows($sql) == 0){
+				header("Location: index.php");
+			}else{
+				$row = mysqli_fetch_assoc($sql);
+			}
+			if(isset($_POST['save'])){
+				$id		     = mysqli_real_escape_string($con,(strip_tags($_POST["id"],ENT_QUOTES)));//Escanpando caracteres 
+				$nombre		     = mysqli_real_escape_string($con,(strip_tags($_POST["nombre"],ENT_QUOTES)));//Escanpando caracteres  
+				$mail	     = mysqli_real_escape_string($con,(strip_tags($_POST["mail"],ENT_QUOTES)));//Escanpando caracteres 
+				$usuario		 = mysqli_real_escape_string($con,(strip_tags($_POST["usuario"],ENT_QUOTES)));//Escanpando caracteres 
+				
+				$update = mysqli_query($con, "UPDATE usuarios SET nombre='$nombre', mail='$mail', usuario='$usuario' WHERE id='$nik'") or die(mysqli_error());
+				if($update){
+					header("Location: edit.php?nik=".$nik."&pesan=sukses");
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error, no se pudo guardar los datos.</div>';
+				}
+			}
+			
+			if(isset($_GET['pesan']) == 'sukses'){
+				echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Los datos han sido guardados con éxito.</div>';
+			}
+			?>
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Id</label>
+					<div class="col-sm-2">
+						<input type="text" name="id" value="<?php echo $row ['id']; ?>" class="form-control" placeholder="NIK" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombre</label>
+					<div class="col-sm-4">
+						<input type="text" name="nombre" value="<?php echo $row ['nombre']; ?>" class="form-control" placeholder="Nombre" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Mail</label>
+					<div class="col-sm-4">
+						<input type="text" name="mail" value="<?php echo $row ['mail']; ?>" class="form-control" placeholder="Mail" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Usuario</label>
+					<div class="col-sm-3">
+						<input type="text" name="usuario" value="<?php echo $row ['usuario']; ?>" class="form-control" placeholder="Usuario" required>
+					</div>
+				</div>
+			
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="save" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+	$('.date').datepicker({
+		format: 'dd-mm-yyyy',
+	})
+	</script>
+</body>
+</html>
